e874fa468e83631c60244634ba4cd7f7f6cb9ae5
Katie Thorpe <kthorpe1023@gmail.com>
2020-08-21 21:37:56 -0600

    Updates

---
+++ precache-manifest.023bd2066e79a513afae557cc963cbd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ce0103c85b70ea6ae29d64974e5f6b",
+    "url": "/kbt-portfolio/index.html"
+  },
+  {
+    "revision": "70a2d91e52fb37db0dfd",
+    "url": "/kbt-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9a23a973a0dfcceb4447",
+    "url": "/kbt-portfolio/static/js/2.cf31c0cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kbt-portfolio/static/js/2.cf31c0cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a2d91e52fb37db0dfd",
+    "url": "/kbt-portfolio/static/js/main.7cb873ab.chunk.js"
+  },
+  {
+    "revision": "bf1aa9e434257aabd90b",
+    "url": "/kbt-portfolio/static/js/runtime-main.ccc6fd41.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kbt-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
