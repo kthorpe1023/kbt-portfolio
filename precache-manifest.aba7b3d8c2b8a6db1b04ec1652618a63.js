60142c0389df8eb11c80c8b811b22a006098ca27
Katie Thorpe <kthorpe1023@gmail.com>
2020-09-05 14:03:53 -0600

    Updates

--- precache-manifest.aba7b3d8c2b8a6db1b04ec1652618a63.js
+++ precache-manifest.aba7b3d8c2b8a6db1b04ec1652618a63.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d5b1c81a09656695b4bf02062ced7895",
+    "revision": "556d0e3ef7e2c319656afaa524b23190",
     "url": "/kbt-portfolio/index.html"
   },
   {
-    "revision": "6a31907da16c40d58afe",
-    "url": "/kbt-portfolio/static/css/main.9105244e.chunk.css"
+    "revision": "dadd0f16e3d5cef6e3a1",
+    "url": "/kbt-portfolio/static/css/main.2aa591ba.chunk.css"
   },
   {
     "revision": "9a23a973a0dfcceb4447",
@@ -16,15 +16,23 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/kbt-portfolio/static/js/2.cf31c0cf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6a31907da16c40d58afe",
-    "url": "/kbt-portfolio/static/js/main.4777a823.chunk.js"
+    "revision": "dadd0f16e3d5cef6e3a1",
+    "url": "/kbt-portfolio/static/js/main.b0236bf0.chunk.js"
   },
   {
     "revision": "bf1aa9e434257aabd90b",
     "url": "/kbt-portfolio/static/js/runtime-main.ccc6fd41.js"
   },
   {
+    "revision": "0f84f50787ef99e9243a26843dad468d",
+    "url": "/kbt-portfolio/static/media/KThorpe.0f84f507.PNG"
+  },
+  {
     "revision": "c0c9ed26ac43e07f4f4af9bb2d3986d9",
     "url": "/kbt-portfolio/static/media/bgimage.c0c9ed26.jpg"
+  },
+  {
+    "revision": "fbd6564aa890b51a5779dcc86495fcc0",
+    "url": "/kbt-portfolio/static/media/inspire.fbd6564a.jpg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kbt-portfolio/precache-manifest.11bc052066146cd3a98fa01928ed7a05.js"
+  "/kbt-portfolio/precache-manifest.aba7b3d8c2b8a6db1b04ec1652618a63.js"
 );
 
 self.addEventListener('message', (event) => {
