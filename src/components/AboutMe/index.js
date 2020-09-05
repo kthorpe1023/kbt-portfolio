474d9596e75b47ba63e8ede318e4d47bc9f4c1bf
Katie Thorpe <kthorpe1023@gmail.com>
2020-09-05 17:22:08 -0600

    images working and card formatting changed

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -4,20 +4,21 @@ import "./style.css"
 const AboutMe = () => {
     return(
       <div className="aboutbg">
-    <div className="container">
-    <div className="row" id="aboutme">
-        <img id="Katie" alt="Me and a mountain"></img>
-  
+    <div  id="aboutme">
+        <img id="katie" src="https://raw.githubusercontent.com/kthorpe1023/kbt-portfolio/master/src/components/AboutMe/KThorpe.PNG" alt="Me in front of the Lennon Wall"></img>
+        </div>
+        <div className="container">
+        <div className="row">
       <div clasName="col s12 col m8">
         <p>I live in Denver, CO, and while working full time as an admissions counselor, I have been learning to code in DU's full stack coding bootcamp.</p>
         <p>Some of my favorite projects to work on were the group projects, Pantry Pirates and Code Connect. You can learn more about those projects and others I have worked on in my portfolio. Feel free to follow my journey as well by <a href="./images/KatieThorpesResumeGH.pdf" target="_blank">looking at my updated resume</a>.</p>
         <p>When I'm not coding, I love getting out to the mountains and hiking. I really value time with my family and close friends. Gameboard nights, time spent in with a book, and rewatching Parks and Recreation are a favorite past time of mine. Put me in front of a puzzle or a project and I will dive into it for hours. I also have a cat that is the poster child for cat obesity, Harold, and he is very wonderful. Thanks for reading!
         </p>
-        
       </div>
       </div>
       </div>
       </div>
+
     )
 }
 
--- src/components/Project/project.js
+++ src/components/Project/project.js
@@ -4,15 +4,40 @@ import "./project.css"
 const Project = (props) => {
 
     return(
-<div className="card mb-3 size">
+<div class="card">
+  <div class="card-image waves-effect waves-block waves-light">
+    <img class="activator" src={props.image} alt={props.name} />
+  </div>
+  <div class="card-content">
+    <span class="card-title activator grey-text text-darken-4">{props.name}<i class="material-icons right">more_vert</i></span>
+  </div>
+  <div class="card-reveal">
+    <span class="card-title grey-text text-darken-4">{props.name}<i class="material-icons right">close</i></span>
+    <ul>
+          <li className="description">
+            <strong>Description:</strong> {props.description}
+          </li>
+          <li>
+            <a href={props.link} target="_blank" rel="noopener noreferrer"><strong>Review the Website</strong></a>
+          </li>
+          <li>
+            <a href={props.ghlink} target="_blank" rel="noopener noreferrer"><strong>Github Repo</strong></a>
+          </li>
+        </ul>
+  </div>
+</div>
+/* <div className="card mb-3 size">
   <div className="row no-gutters">
     <div className="col-md-12">
       {console.log(props.image)}
-      <img src={props.image}  className="card-img-top img-size" alt={props.name}  />
+      <img src={props.image}  className="activator card-img-top img-size" alt={props.name}  />
     </div>
     <div className="col-md-12">
       <div className="card-body">
-        <h5 className="card-title">Name: {props.name}</h5>
+        <h5 className="card-title activator">Name: {props.name}</h5>
+        </div>
+        <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
         <ul>
         <li className="description">
           <strong>Description:</strong> {props.description}
@@ -25,11 +50,13 @@ const Project = (props) => {
         </li>
       </ul>
       </div>
+      </div>
     </div>
-  </div>
-</div>
+  </div> */
     )
     
 }
+
+
 
 export default Project
