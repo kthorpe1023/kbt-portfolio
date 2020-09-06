ffa81431970862f233b0ad5f74ec864079395d4d
Katie Thorpe <kthorpe1023@gmail.com>
2020-09-06 10:38:32 -0600

    updated about me and changed portfolio descriptions

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -8,7 +8,8 @@ const Contact = () => {
             <h4>Contact Me:</h4>
             <h6>kthorpe1023@gmail.com</h6>
             <h6><i class="material-icons">location_on</i>Denver, 80246</h6>
-            <a className="resume" href={pdf} target="_blank">Checkout my Resume</a>
+            {/* linkprop styling defined in AboutMe style.css */}
+            <a className="resume linkprop" href={pdf} target="_blank">Checkout my Resume</a>
         </div>
     )
 }
--- src/components/Social/social.js
+++ src/components/Social/social.js
@@ -4,9 +4,10 @@ import "./social.css"
 const Social = () => {
     return(
         <section className="social container">
-        <a href="https://www.linkedin.com/in/katie-thorpe-343064181/" target="_blank" rel="noopener noreferrer" className="fa fa-linkedin col 2"></a>
-        <a href="https://github.com/kthorpe1023" target="_blank" rel="noopener noreferrer" className="fa fa-github col 2"></a>
-        <a href="https://www.instagram.com/ktbrady1023/" target="_blank" rel="noopener noreferrer" className="fa fa-instagram col 2"></a>
+            {/* linkprop styling defined in AboutMe style.css file */}
+        <a href="https://www.linkedin.com/in/katie-thorpe-343064181/" target="_blank" rel="noopener noreferrer" className="fa fa-linkedin col 2 linkprop"></a>
+        <a href="https://github.com/kthorpe1023" target="_blank" rel="noopener noreferrer" className="fa fa-github col 2 linkprop"></a>
+        <a href="https://www.instagram.com/ktbrady1023/" target="_blank" rel="noopener noreferrer" className="fa fa-instagram col 2 linkprop"></a>
         </section>
     )
 }
